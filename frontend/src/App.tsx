032f544f7f3ac6a4795fd7d6d3d521e2e02ee3f9
tranker123 <fogerdance2015@gmail.com>
2022-09-07 23:54:25 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return(
+    <>
     <h1>Ola mundo!</h1>
+    <NotificationButton />
+    <NotificationButton />
+    </>
   )
 }
 
